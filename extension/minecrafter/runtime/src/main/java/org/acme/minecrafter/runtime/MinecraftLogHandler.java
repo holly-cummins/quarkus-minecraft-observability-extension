b10670685a3ffb4cd1e702a900c638fed321fab6
Holly Cummins <hcummins@redhat.com>
2022-05-31 18:48:35 +0100

    Tidy unused code

--- extension/minecrafter/runtime/src/main/java/org/acme/minecrafter/runtime/MinecraftLogHandler.java
+++ extension/minecrafter/runtime/src/main/java/org/acme/minecrafter/runtime/MinecraftLogHandler.java
@@ -1,17 +1,9 @@
 package org.acme.minecrafter.runtime;
 
-import java.util.Arrays;
 import java.util.logging.Handler;
 import java.util.logging.LogRecord;
 
 public class MinecraftLogHandler extends Handler {
-
-    private String logStreamName;
-    private String logGroupName;
-    private String sequenceToken;
-
-    // ...
-
 
     @Override
     public void publish(LogRecord record) {
